fad179385d0900e3cd0d28278c7175215015e5ea
Emiliano Errecalde <emilianoerrecalde@Emilianos-MacBook-Pro.local>
2026-08-20 23:34:53 +0100

    refactor: remove inactive legacy route bundle

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { Icon, PetImage, PetSeenMark, Progress } from './ui'
-export { LanguagePicker, SimpleHeader, SiteFooter, SiteHeader } from './layout'
-export { PetAttributeSuggestions } from './PetAttributeSuggestions'
--- src/shared/paths.ts
+++
@@ -1,6 +0,0 @@
-import { defaultLocale, localeUrlPrefix } from '../i18n'
-import type { AppLocale } from '../i18n/resources'
-
-export function localisedPath(path: string, locale: string | undefined) {
-  return `${localeUrlPrefix((locale ?? defaultLocale) as AppLocale)}${path}`
-}
